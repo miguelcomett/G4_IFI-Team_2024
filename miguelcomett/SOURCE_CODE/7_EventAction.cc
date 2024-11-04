867d7de97ce026be6f9bb32e523f897566aca92c
miguelcomett <A01735935@tec.mx>
2024-11-03 19:01:34 -0600

    .

--- miguelcomett/SOURCE_CODE/7_EventAction.cc
+++ miguelcomett/SOURCE_CODE/7_EventAction.cc
@@ -13,18 +13,12 @@ void MyEventAction::EndOfEventAction(const G4Event * )
 { 
     fEDep = fEDep / keV;
 
-    if (arguments == 1)
-    {
-        G4int Event = G4RunManager::GetRunManager() -> GetCurrentEvent() -> GetEventID();
-        // G4cout << "Energy deposition (keV): " << fEDep << "  |Event ID: " << Event << G4endl; 
-    }
-    G4int Event = G4RunManager::GetRunManager() -> GetCurrentEvent() -> GetEventID();
-
     if (fEDep == 0.0){return;}
-    G4cout << "Energy deposition (keV): " << fEDep << "  |Event ID: " << Event << G4endl; 
+    G4int Event = G4RunManager::GetRunManager() -> GetCurrentEvent() -> GetEventID();
+    // G4cout << "Energy deposition (keV): " << fEDep << "  |Event ID: " << Event << G4endl; 
+
     G4AnalysisManager * analysisManager = G4AnalysisManager::Instance();
 
-    
     if (arguments == 1 || arguments == 2)
     {
         analysisManager -> FillNtupleDColumn(1, 0, fEDep);

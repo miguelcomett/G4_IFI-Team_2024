71eb7c32cbb78648219b5b23a7b6a69835522d5e
Juan Pablo Solís Ruiz <A01067387@tec.mx>
2024-11-07 16:21:12 -0600

    Listo, ya sirve. Desintalen apple

--- MedX_Engine/SOURCE_CODE/6.0_RunAction.cc
+++ MedX_Engine/SOURCE_CODE/6.0_RunAction.cc
@@ -248,7 +248,12 @@ void MyRunAction::MergeRootFiles()
     #endif
 
     // Nueva ruta para Output/ fuera de ROOT/
-    std::string outputDirectory = std::filesystem::path(currentPath).parent_path().string() + "/ROOT/";
+        // Modificado: La carpeta Output se moverá al mismo nivel que ROOT
+    #ifdef __APPLE__
+            std::string outputDirectory = std::filesystem::path(currentPath).string() + "/ROOT/";
+    #else
+            std::string outputDirectory = std::filesystem::path(currentPath).parent_path().string() + "/ROOT/";
+    #endif
 
     // Crear la carpeta Output si no existe
     if (!std::filesystem::exists(outputDirectory))
